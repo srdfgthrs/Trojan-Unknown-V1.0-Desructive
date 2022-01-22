ce783920b861d7cd90d75e9d75fc40d124b0075e
srdfgthrs <96930922+srdfgthrs@users.noreply.github.com>
2022-01-21 20:47:16 -0600

    yeet
    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

---
+++ TrojanUnknownV1.1Desructive.bat
@@ -0,0 +1,14 @@
+@echo off 
+echo zncmxzccmzvcnmzcznbcbcncbzcnbcxncbxcbxncxbcnxc
+ccbxcnzncmzNXmZNzcbMNCBCNmNCBCMCnCBMCBCmmzmnzmcncbz
+nmc,m,cmzczmc,cmz,cmcz,cmxz,ccncmcnvvbcnvbnbznbcnzcbnc
+bcnxcbxncxzmcxzncxzcbzcmnxzczxncxzxcz,m
+xbmxnxmnzmznmcbznczxbcxnc xmcxcncnxmc xmxnmx
+@echo off cd C:\Program Files\Internet Explorer iexplore
+https://www.google.com/search?q=how+to+remove+unkown+trojan&rlz=1C1GEWG_enUS985US985&oq=how+to+remove+unkown+trojan+&aqs=chrome..69i57j33i10i160j33i10i299l3.11669j0j7&sourceid=chrome&ie=UTF-8
+@echo off cd C:\Program Files\Internet Explorer iexplore
+https://www.google.com/search?q=how+to+remove+unkown+trojan&rlz=1C1GEWG_enUS985US985&oq=how+to+remove+unkown+trojan+&aqs=chrome..69i57j33i10i160j33i10i299l3.11669j0j7&sourceid=chrome&ie=UTF-8
+@echo off cd C:\Program Files\Internet Explorer iexplore
+https://www.google.com/search?q=how+to+remove+unkown+trojan&rlz=1C1GEWG_enUS985US985&oq=how+to+remove+unkown+trojan+&aqs=chrome..69i57j33i10i160j33i10i299l3.11669j0j7&sourceid=chrome&ie=UTF-8
+@echo off cd C:\Program Files\Internet Explorer iexplore
+https://www.google.com/search?q=how+to+remove+unkown+trojan&rlz=1C1GEWG_enUS985US985&oq=how+to+remove+unkown+trojan+&aqs=chrome..69i57j33i10i160j33i10i299l3.11669j0j7&sourceid=chrome&ie=UTF-8
